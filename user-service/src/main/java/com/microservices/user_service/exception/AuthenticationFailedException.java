eea0b73d8b2e078bd5ef4bf30c6374fa9914d783
J. Venkat Vardhan <115476518+VenkyAA@users.noreply.github.com>
2025-03-07 21:54:44 +0530

    Delete user-service directory

--- user-service/src/main/java/com/microservices/user_service/exception/AuthenticationFailedException.java
+++
@@ -1,8 +0,0 @@
-package com.microservices.user_service.exception;
-
-public class AuthenticationFailedException extends RuntimeException {
-    public AuthenticationFailedException(String message) {
-        super(message);
-    }
-}
-
--- user-service/src/main/java/com/microservices/user_service/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.microservices.user_service.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
-
